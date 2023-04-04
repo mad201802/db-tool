641f465a3915474e0d6d8bb33140305ff23a124c
mad201802 <md148@hdm-stuttgart.de>
2023-04-04 16:01:43 +0200

    working mysql and sqlite table and column retrieval

--- src/main/java/de/dbtool/DatabaseTests.java
+++ src/main/java/de/dbtool/DatabaseTests.java
@@ -8,26 +8,43 @@ public class DatabaseTests {
     private Connection conn;
 
     public static void main(String[] args) throws SQLException {
-        DatabaseTests dbTests = new DatabaseTests("jdbc:sqlite:test.sqlite");
-        System.out.println(dbTests.getTables());
+        DatabaseTests dbTests = new DatabaseTests("jdbc:sqlite:src\\main\\resources\\test.sqlite");
+//        DatabaseTests dbTests = new DatabaseTests("jdbc:mysql://192.168.178.60:6033/database", "admin", "admin");
+        dbTests.printAllTables();
+        System.out.println("====================================");
+        dbTests.printAllTableColumns("tasks");
     }
 
     public DatabaseTests(String connectionString) throws SQLException {
          this.conn = DriverManager.getConnection(connectionString);
     }
 
-    public ArrayList<String> getTables() throws SQLException {
-        ArrayList<String> result = new ArrayList<>();
+    public DatabaseTests(String connectionString, String username, String password) throws SQLException {
+        this.conn = DriverManager.getConnection(connectionString, username, password);
+    }
+
+    public void printAllTables() throws SQLException {
         //Retrieving the meta data object
         DatabaseMetaData metaData = this.conn.getMetaData();
         String[] types = {"TABLE"};
         //Retrieving the columns in the database
         ResultSet tables = metaData.getTables(null, null, "%", types);
         while (tables.next()) {
-            result.add(tables.getString("TABLE_NAME"));
+            System.out.println(tables.getString("TABLE_NAME"));
         }
+    }
 
-        return result;
+    public void printAllTableColumns(String tableName) throws SQLException {
+        ArrayList<String> result = new ArrayList<>();
+        DatabaseMetaData metadata = this.conn.getMetaData();
+        ResultSet resultSet = metadata.getColumns(null, null, tableName, null);
+        while (resultSet.next()) {
+            String name = resultSet.getString("COLUMN_NAME");
+            String type = resultSet.getString("TYPE_NAME");
+            int size = resultSet.getInt("COLUMN_SIZE");
+
+            System.out.println("Column name: [" + name + "]; type: [" + type + "]; size: [" + size + "]");
+        }
     }
 
 
