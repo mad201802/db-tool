024032c3684af629971e510d76bc431da40ba943
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2023-08-13 17:30:23 +0200

    #47 Implemented `list-tables` command

--- src/main/java/de/dbtool/cli/DbToolCommand.java
+++ src/main/java/de/dbtool/cli/DbToolCommand.java
@@ -3,13 +3,14 @@ package de.dbtool.cli;
 import de.dbtool.cli.subcommands.CreateProfileCommand;
 import de.dbtool.cli.subcommands.GrepCommand;
 import de.dbtool.cli.subcommands.ListProfilesCommand;
+import de.dbtool.cli.subcommands.ListTablesCommand;
 import picocli.CommandLine;
 import picocli.CommandLine.Model.CommandSpec;
 
 /**
  * Main command for the db-tool
  */
-@CommandLine.Command(name = "db-tool", version = "0.0.1", description = "Tool to help manage databases", mixinStandardHelpOptions = true, subcommands = {CreateProfileCommand.class, ListProfilesCommand.class, GrepCommand.class})
+@CommandLine.Command(name = "db-tool", version = "0.0.1", description = "Tool to help manage databases", mixinStandardHelpOptions = true, subcommands = {CreateProfileCommand.class, ListProfilesCommand.class, GrepCommand.class, ListTablesCommand.class})
 public class DbToolCommand implements Runnable {
 
     @CommandLine.Spec
